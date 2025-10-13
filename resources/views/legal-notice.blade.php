88ced35c144f3f19e6eadd68600b6f4d6777eb3f
Ryan Hellyer <ryanhellyer@gmail.com>
2025-10-13 19:30:00 +0100

    feat: update views and layouts for analytics display

--- resources/views/legal-notice.blade.php
+++ resources/views/legal-notice.blade.php
@@ -3,38 +3,38 @@
 @section('body-class', 'legal-notice')
 @section('page-title', 'Legal Notice')
 @section('page-tagline', 'Unimportant mumbo jumbo for legal purposes')
-@section('title', 'Legal Notice - Spam Annihilator')
-@section('description', 'Legal notice and contact information for Spam Annihilator. Find operator details, contact information, and legal information.')
+@section('title', 'Legal Notice - Spam Destroyer')
+@section('description', 'Legal notice and contact information for Spam Destroyer. Find operator details, contact information, and legal information.')
 @section('robots', 'noindex')
-@section('og:title', 'Legal Notice - Spam Annihilator')
-@section('og:description', 'Legal notice and contact information for Spam Annihilator. Find operator details, contact information, and legal information.')
+@section('og:title', 'Legal Notice - Spam Destroyer')
+@section('og:description', 'Legal notice and contact information for Spam Destroyer. Find operator details, contact information, and legal information.')
 @section('canonical', url('/legal-notice/'))
 
 @section('header-button')
-<a href="/home-new/" class="button">Back to Home</a>
+<a href="/" class="button">Back to Home</a>
 @endsection
 
 @section('header-form')
 @endsection
 
 @section('content')
-  <section class="primary">
-    <div class="wrapper">
-      <h2>Website Operator</h2>
-      
-      <h3>Provider/Operator:</h3>
-      <p>
-        Ryan Hellyer<br>
-        Friedrichstraße 114a<br>
-        Berlin 10117<br>
-        Germany
-      </p>
+	<section class="primary">
+		<div class="wrapper">
+			<h2>Website Operator</h2>
+			
+			<h3>Provider/Operator:</h3>
+			<p>
+				Ryan Hellyer<br>
+				Friedrichstraße 114a<br>
+				Berlin 10117<br>
+				Germany
+			</p>
 
-      <h3>Contact Details:</h3>
-      <p>
-        Telephone: <a href="tel:+491745390977">+49 174 5390977</a><br>
-        E-Mail: <a href="mailto:ryan@hellyer.kiwi">ryan@hellyer.kiwi</a>
-      </p>
-    </div>
-  </section>
+			<h3>Contact Details:</h3>
+			<p>
+				Telephone: <a href="tel:+491745390977">+49 174 5390977</a><br>
+				E-Mail: <a href="mailto:ryan@hellyer.kiwi">ryan@hellyer.kiwi</a>
+			</p>
+		</div>
+	</section>
 @endsection
