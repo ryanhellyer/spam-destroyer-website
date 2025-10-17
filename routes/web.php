b3b4b150d7cc23034f1cfb4d8f407263ee6b7198
Ryan Hellyer <ryanhellyer@gmail.com>
2025-10-17 19:48:00 +0100

    feat: update route definitions

--- routes/web.php
+++ routes/web.php
@@ -5,14 +5,10 @@ use App\Http\Controllers\RedirectManagementController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 Route::post('/', [RedirectManagementController::class, 'store'])->name('redirect.store');
-
-Route::get('/home-new/', function () {
-    return view('home');
-});
 
 Route::get('/legal-notice/', function () {
     return view('legal-notice');
