9d5e994f60a8e2771a487dbbf0f21803a7e9d96e
Ryan <ryanhellyer@gmail.com>
2025-11-18 20:30:00 +0100

    feat: add HasFactory to models for testing

--- app/Models/PathCounter.php
+++ app/Models/PathCounter.php
@@ -4,10 +4,14 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class PathCounter extends Model
 {
+    /** Enables factory support for tests (PathCounter::factory()) */
+    use HasFactory;
+
     protected $fillable = [
         'path',
         'hit_count',
--- app/Models/UrlMapping.php
+++ app/Models/UrlMapping.php
@@ -4,10 +4,14 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class UrlMapping extends Model
 {
+    /** Enables factory support for tests (UrlMapping::factory()) */
+    use HasFactory;
+
     protected $casts = [
         'hit_count' => 'integer',
     ];
